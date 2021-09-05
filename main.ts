875116275d825a9350c8da2b5b1ebf8ae005bbf4
Maria Muravyova <avejant0@gmail.com>
2021-09-05 21:05:53 +0300

    Removed tests from main and added implementation statuses instead.

--- main.ts
+++ main.ts
@@ -1,117 +1,52 @@
-import { 
-  Button,
-  Checkbox,
-  ExcelReportFactory,
-  ImageFieldBuilder,
-  MySqlQueryExecutor,
-  PdfReportFactory,
-  PgQueryExecutor,
-  SelectFieldBuilder,
-  Singleton, 
-  TextFieldBuilder 
-} from "./patterns/creational";
+const groups = [
+  {
+    name: 'Creational',
+    patterns: [
+      { name: 'Factory Method', isDone: true },
+      { name: 'Abstract Factory', isDone: true },
+      { name: 'Prototype', isDone: true },
+      { name: 'Builder', isDone: true },
+      { name: 'Singleton', isDone: true },
+    ]
+  },
+  {
+    name: 'Structural',
+    patterns: [
+      { name: 'Adapter', isDone: false },
+      { name: 'Bridge', isDone: false },
+      { name: 'Composite', isDone: false },
+      { name: 'Decorator', isDone: false },
+      { name: 'Facade', isDone: false },
+      { name: 'Flyweight', isDone: false },
+      { name: 'Proxy', isDone: false },
+    ]
+  },
+  {
+    name: 'Behavioral',
+    patterns: [
+      { name: 'Chain of Responsibilities', isDone: false },
+      { name: 'Command', isDone: false },
+      { name: 'Iterator', isDone: false },
+      { name: 'Mediator', isDone: false },
+      { name: 'Memento', isDone: false },
+      { name: 'Observer', isDone: false },
+      { name: 'State', isDone: false },
+      { name: 'Strategy', isDone: false },
+      { name: 'Template Method', isDone: false },
+      { name: 'Visitor', isDone: false },
+    ]
+  }
+]
 
-// Singleton test
-console.log('\n*** SINGLETONE TEST ***\n')
-const instance1 = Singleton.getInstance();
+console.log('IMPLEMENTATION PROGRESS \n');
+console.log('================\n');
 
-// Will raise error if will be called
-// const instance2 = new Singleton();
-const instance3 = Singleton.getInstance();
+for (const group of groups) {
+  console.log(`${group.name}:\n`);
+  for (const pattern of group.patterns) {
+    const mark = pattern.isDone ? 'X' : ' ';
+    console.log(` - [${mark}] - ${pattern.name}`); 
+  }
 
-// Assert
-const counter1 = instance1.getCounter();
-const counter3 = instance3.getCounter();
-if (counter1 !== counter3) {
-  throw new Error(`Expected ${counter1} equals ${counter3} but it was not.`);
-} else {
-  console.log('Singleton works correctly.');
+  console.log('\n================\n');
 }
-
-// End of Singleton test
-
-// Builder test
-console.log('\n*** BUILDER TEST ***\n')
-
-const textBuilder = new TextFieldBuilder();
-const selectBuilder = new SelectFieldBuilder();
-const imageBuilder = new ImageFieldBuilder();
-
-const textField1 = textBuilder
-  .setTitle('First Text Field')
-  .setType()
-  .build();
-
-const textField2 = textBuilder
-  .setTitle('Second Text Field')
-  .setType()
-  .build();
-
-const selectField = selectBuilder
-  .setTitle('Select Field')
-  .setType()
-  .setOptions(['One', 'Two', 'Three'])
-  .build();
-
-const imageField = imageBuilder
-  .setTitle('Image Field')
-  .setType()
-  .setSize({ width: 100, height: 100 })
-  .build();
-
-console.log(textField1.getInfo());
-console.log(textField2.getInfo());
-console.log(selectField.getInfo());
-console.log(imageField.getInfo());
-// End of Builder test
-
-// Factory Method test
-console.log('\n*** FACTORY METHOD TEST ***\n')
-
-const executor1 = new PgQueryExecutor();
-const executor2 = new MySqlQueryExecutor();
-
-executor1.execute('test 1');
-executor2.execute('test 2');
-// End of Factory Method test
-
-// Abstract Factory test
-console.log('\n*** ABSTRACT FACTORY METHOD TEST ***\n')
-
-const excelFactory = new ExcelReportFactory();
-const pdfFactory = new PdfReportFactory();
-
-const annualReport1 = excelFactory.createAnnualReport();
-const annualReport2 = pdfFactory.createAnnualReport();
-
-const userReport1 = excelFactory.createReportForUser("john.doe");
-const userReport2 = pdfFactory.createReportForUser("mary.moore");
-
-console.log(annualReport1.getTotal());
-console.log(annualReport2.getTotal());
-console.log(userReport1.getInfo());
-console.log(userReport2.getInfo());
-// End of Abstract Factory test
-
-// Prototype test
-console.log('\n*** PROTOTYPE TEST ***\n')
-const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
-
-const button = new Button(10,20,30,40);
-const checkbox = new Checkbox(false);
-
-delay(2000).then(() => {
-  const buttonClone = button.clone();
-  const checkboxClone = checkbox.clone();
-  
-  console.log(buttonClone.getStatus());
-  console.log(checkboxClone.getStatus());
-  
-  console.log(button.getTimeStamp());
-  console.log(buttonClone.getTimeStamp());
-  
-  console.log(checkbox.getTimeStamp());
-  console.log(checkboxClone.getTimeStamp());
-});
-
-// End of Prototype test
